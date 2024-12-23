fdc452c5c3b8ecb1213124fc757c876826f4feaa
Agarvv <casluagarv@gmail.com>
2024-12-23 15:11:47 +0100

    issues

--- client/src/features/car-lists/CarLists.tsx
+++ client/src/features/car-lists/CarLists.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import CarList from './car-list/CarList'
-import styles from './CarLists.module.css'
-
+import React from 'react';
+import CarList from './car-list/CarList';
+import styles from './CarLists.module.css';
 
 const CarLists = () => {
-    const carList = [
-      'BMW',
-      'Porsche',
-      'Mercedes'
-    ]
-    return(
-      <>
-        {carList.map(carList => (
-          <span>{ carList }</span> 
-          <div key={car}> 
-          <CarList
-          brand={carList}
-          /> 
-          </div> 
-        ))}
-      </> 
-    )
-}
+  const carList = [
+    'BMW',
+    'Porsche',
+    'Mercedes'
+  ];
 
-export default CarLists; 
+  return (
+    <>
+      {carList.map(car => (
+        <div key={car} className={styles.carItem}>
+          <span>{car}</span> 
+          <CarList
+            brand={car}
+          /> 
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CarLists;

beb29fc6a4ed3931bb7a6c649effdca266090b26
Agarvv <casluagarv@gmail.com>
2024-12-24 14:34:08 +0100

    Auth middleware

---
+++ server/lebagarv/Presentation/Middleware/AuthMiddleware.cs
@@ -0,0 +1,33 @@
+namespace lebagarv.Presentation.Middleware; 
+
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using lebagarv.Infrastructure.Security; 
+
+public class AuthMiddleware 
+{
+    private readonly RequestDelegate _next; 
+    private readonly JwtService _jwtService; 
+    
+    public AuthMiddleware(RequestDelegate next, JwtService jwtService)
+    {
+        this._next = next; 
+        this._jwtService = jwtService; 
+    }
+    
+    public async Task InvoqueAsync(HttpContext context)
+    {
+        var jwtCookie = context.Request.Cookies["jwt"]; 
+        if(string.isNullOrEmpty(jwtCookie))
+        {
+            // debug 
+            Console.WriteLine("JWT cookie not found"); 
+        }
+        
+        var claimsPrincipal = _jwtService.ValidateToken(jwt); 
+        Console.WriteLine("JWT validated", claimsPrincipal); 
+
+        await _next(context); 
+    }
+}

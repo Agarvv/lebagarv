b1ec734fd63017b4ee2e223cdd9d76337b2346b8
Agarvv <casluagarv@gmail.com>
2024-12-24 13:18:59 +0100

    usePost hook

---
+++ client/src/hooks/usePost.ts
@@ -0,0 +1,37 @@
+import { useMutation } from '@tanstack/react-query';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setLoading, setSuccess, setError } from 'src/store/apiStatus/apiStatusSlice';
+import { AppDispatch } from 'src/store/index';
+
+/* example of use 
+const { mutate } = usePost<FormValue>(data, successMessage, true) 
+mutate()
+*/
+export const usePost = <T,>(
+  data: T,
+  successMessage: string,
+  withError: boolean,
+  serviceFunc: (data: T) => Promise<any>
+) => {
+  const dispatch: AppDispatch = useDispatch();
+
+  return useMutation({
+    mutationFn: () => serviceFunc(data),
+    onMutate: () => {
+      dispatch(setLoading());
+    },
+    onError: (error: Error) => {
+      console.error(error);
+      if (withError) dispatch(setError('Something went wrong...'));
+    },
+    onSuccess: (response: any) => {
+      if (successMessage) {
+        dispatch(setSuccess(successMessage));
+      } else {
+        console.log('Post succeeded');
+      }
+      return response; 
+    },
+  });
+};

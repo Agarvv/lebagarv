4b589ecf96272cc0b1ce3b80af8320d8b2c612bb
Agarvv <casluagarv@gmail.com>
2025-01-02 22:12:12 +0100

    api url issues

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api/chatssy`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api/lebagarv`,
   timeout: 10000
 });
 // .env 

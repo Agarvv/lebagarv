88af58b6dc3e6d9b272890284f8349648ace8c98
Agarvv <casluagarv@gmail.com>
2025-01-03 19:46:20 +0100

    issues

--- client/src/types/cars/CarDetails.ts
+++ client/src/types/cars/CarDetails.ts
@@ -1,3 +1,13 @@
 export type CarDetails = {
-    
+    id: number,
+    title: string,
+    price: number,
+    images: string[],
+    carYear: number,
+    fuelType: string,
+    gearbox: string,
+    date: string,
+    city: string,
+    brand: string,
+    color: string 
 }

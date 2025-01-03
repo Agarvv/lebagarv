d013204023d00e36c89bb5372252c95a36377f47
Agarvv <casluagarv@gmail.com>
2025-01-03 18:12:22 +0100

    issues

--- client/src/api/services/cars/CarService.ts
+++ client/src/api/services/cars/CarService.ts
@@ -1,13 +1,15 @@
 import axiosInstance from "src/config/axiosConfig";
+import { CarShowcase } from "src/types/car/CarShowcase";
 
-export const createCar = <T>(data: T) => {
-    return axiosInstance.post('/cars/create', data, {
-        withCredentials: true 
+export const createCar = async <T>(data: T): Promise<void> => {
+    await axiosInstance.post('/cars/create', data, {
+        withCredentials: true,
     });
-}
+};
 
-export const getCars = () => {
-    return axiosInstance.get('/cars', {
-        withCredentials: true 
+export const getCars = async (): Promise<CarShowcase[]> => {
+    const response = await axiosInstance.get('/cars', {
+        withCredentials: true,
     });
-}
+    return response.data; 
+};

260cdd800c40788eadc41ad1f0bdac2006b5dce3
Agarvv <casluagarv@gmail.com>
2025-01-03 21:58:03 +0100

    issues

--- client/src/features/car-details/car-data/CarData.tsx
+++ client/src/features/car-details/car-data/CarData.tsx
@@ -65,31 +65,31 @@ const CarData: React.FC<Props> = ({ car }: Props) => {
                   <CarDetail 
                       icon={<i className="fa fa-bars"></i>}
                       detail="Car Doors"
-                      detailValue="5 doors"
+                      detailValue={car?.doors}
                   />
 
                   <CarDetail 
                       icon={<i className="fa fa-chair"></i>}
                       detail="Car Seats"
-                      detailValue="5 Seats"
+                      detailValue={car?.seats}`
                   />
 
                   <CarDetail 
                       icon={<i className="fa fa-bolt"></i>}
                       detail="Horsepower"
-                      detailValue="0 HP"
+                      detailValue={car?.horsepower}
                   />
 
                   <CarDetail 
                       icon={<i className="fa fa-tachometer"></i>}
                       detail="Kilometers"
-                      detailValue="000.000"
+                      detailValue={car?.kilometers}
                   />
               </div>
 
               <h4 className="cd-p">Car Description</h4>
               <div className={styles.carDescription}>
-                  <p>Du bist wie ein kostbarer Edelstein, der in der Dunkelheit funkelt, doch manchmal verstehst du die Tiefe deiner eigenen Strahlkraft nicht. Dein Herz ist wie ein unbeschriebenes Buch, voller unentdeckter Geschichten, die nur darauf warten, erzählt zu werden. Weißt du, dass du eine unendliche Quelle von Möglichkeiten bist, die nur darauf wartet, freigesetzt zu werden.</p>
+                  <p>{car?.description ?? 'No description provided'}</p>
               </div>
           </div>
       </div>

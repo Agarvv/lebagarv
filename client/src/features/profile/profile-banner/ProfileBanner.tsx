21fd16801449a7b42f5d62abc5effe128b628904
Agarvv <casluagarv@gmail.com>
2025-01-04 21:17:46 +0100

    working on rendering user cars

--- client/src/features/profile/profile-banner/ProfileBanner.tsx
+++ client/src/features/profile/profile-banner/ProfileBanner.tsx
@@ -3,11 +3,12 @@ import { isSelfProps } from '../types';
 import styles from './ProfileBanner.module.css';
 import logo from 'src/logo.svg';
 import useImageUpload from 'src/hooks/useImageUpload';
-import useSetProfileBanner from 'src/hooks/useSetProfileBanner';
+import { usePost } from 'src/hooks/usePost';
+import { setUserProfileBanner } from 'src/api/services/profile/ProfileService';
 
 const ProfileBanner: React.FC<isSelfProps> = ({ isSelf, valueToDisplay }) => {
     const { imageUrl, uploadImage } = useImageUpload();
-    const { mutate } = useSetProfileBanner();  
+    const { mutate } = usePost<string>("Profile Banner Updated !", true, setUserProfileBanner);  
     const fileInputRef = React.useRef<HTMLInputElement>(null);
 
     const handleBannerChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,7 +18,7 @@ const ProfileBanner: React.FC<isSelfProps> = ({ isSelf, valueToDisplay }) => {
                 const image = await uploadImage(file, 'image');
                 console.log('Banner uploaded successfully!', image);
                 mutate(image); 
-                window.location.reload()
+                //window.location.reload()
             } catch (error) {
                 console.error('Error uploading banner:', error);
             }

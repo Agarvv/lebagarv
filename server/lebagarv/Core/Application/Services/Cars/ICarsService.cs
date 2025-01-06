279148aec60e16e1a7e994c56eaa0dc23ca9353d
Agarvv <casluagarv@gmail.com>
2025-01-06 22:41:22 +0100

    issues

--- server/lebagarv/Core/Application/Services/Cars/ICarsService.cs
+++ server/lebagarv/Core/Application/Services/Cars/ICarsService.cs
@@ -8,5 +8,5 @@ public interface ICarsService
     public Task<IEnumerable<Car>> GetCarsAsync();
     public Task<IEnumerable<Car>> SearchCarsAsync(string query);
     public Task<bool> CreateCarAsync(CreateCarRequest request, int userId); 
-    public Task<Car> GetCarByIdAsync(int id, int userId); 
+    public Task<CarDTO> GetCarByIdAsync(int id, int userId); 
 }

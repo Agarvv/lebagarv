20a752f8002807cfc38dc052fc5f3879c216de22
Agarvv <casluagarv@gmail.com>
2025-01-06 18:43:36 +0100

    issues

--- client/src/layout/car/add-to-favorite/AddToFavorite.tsx
+++ client/src/layout/car/add-to-favorite/AddToFavorite.tsx
@@ -1,32 +1,29 @@
-import React from 'react' 
-import styles from './AddToFavorite.module.css'
-import { usePost } from 'src/hooks/usePost'
-import { addOrRemoveFavorite } from 'src/api/services/favorites/FavoritesService'
+import React from 'react';
+import styles from './AddToFavorite.module.css';
+import { usePost } from 'src/hooks/usePost';
+import { addOrRemoveFavorite } from 'src/api/services/favorites/FavoritesService';
 
 interface Props {
-    carId: number,
-    isFavorite: boolean 
+  carId: number;
+  isFavorite: boolean;
 }
 
-const AddToFavorite: React.FC = () => {
-    const { mutate } = usePost<number>
-    (
-      () => addOrRemoveFavorite(carId),
-      "",
-      true 
-    )
+const AddToFavorite: React.FC<Props> = ({ carId, isFavorite }) => {
+  const { mutate } = usePost<number>(
+    'Favorite updated successfully', // successMessage
+    true, // withError
+    addOrRemoveFavorite // serviceFunc
+  );
 
-    const handleFavoritesClick = () => {
-        mutate(carId)
-    }
+  const handleFavoritesClick = () => {
+    mutate(carId);
+  };
 
-    return(
-      <>
-        <div onClick={handleFavoritesClick} className={styles.favBtn}>
-           <i className="fa fa-heart"></i> 
-        </div>
-      </> 
-    )
-}
+  return (
+    <div onClick={handleFavoritesClick} className={styles.favBtn}>
+      <i className="fa fa-heart"></i>
+    </div>
+  );
+};
 
-export default AddToFavorite; 
+export default AddToFavorite;

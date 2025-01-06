f56edda2c1a5522b50a765874ea1f479d509d553
Agarvv <casluagarv@gmail.com>
2025-01-06 19:32:10 +0100

    issues

--- client/src/features/search/Search.tsx
+++ client/src/features/search/Search.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react' 
 import Cars from 'src/layout/cars/Cars'
 import { useGet } from 'src/hooks/useGet'
-import CarShowcase from 'src/types/cars/CarShowcase'
+import { CarShowcase } from 'src/types/cars/CarShowcase'
 import { search } from 'src/api/services/search/SearchService'
 
 const Search = () => {

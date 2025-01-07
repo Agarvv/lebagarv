be83eb7859af247bd70b7f712e31a816e89b55dd
Agarvv <casluagarv@gmail.com>
2025-01-07 15:33:58 +0100

    Search page polisgsh and Cars component UI fix

--- client/src/features/search/Search.tsx
+++ client/src/features/search/Search.tsx
@@ -3,12 +3,15 @@ import Cars from 'src/layout/cars/Cars'
 import { useGet } from 'src/hooks/useGet'
 import { CarShowcase } from 'src/types/cars/CarShowcase'
 import { search } from 'src/api/services/search/SearchService'
+import { useParams } from 'react-router-dom';
 
 const Search = () => {
+    const { query } = useParams<{query: string}>()
+    
     const { data: results } = useGet<CarShowcase[]>
     (
     {
-        serviceFunc: () => search("bemve"),
+        serviceFunc: () => search(query || ""),
         successFunc: () => console.log("Search success"),
         withError: true 
     }

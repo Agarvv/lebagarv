89cde828c3da720e6e0951bde6140bae611da174
Agarvv <casluagarv@gmail.com>
2025-01-07 16:14:36 +0100

    forgot await

--- server/lebagarv/Presentation/Controllers/Chat/ChatController.cs
+++ server/lebagarv/Presentation/Controllers/Chat/ChatController.cs
@@ -22,7 +22,7 @@ public class ChatController : ControllerBase
     {
         var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
         
-        var chats = _chatService.GetUserChats(userId);
+        var chats = await _chatService.GetUserChats(userId);
         
         return Ok(chats); 
     

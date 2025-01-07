eddb9f287140476352e3e11da59de373447e807c
Agarvv <casluagarv@gmail.com>
2025-01-07 02:53:11 +0100

    fixing issues

--- client/src/features/car-details/car-data/add-or-remove-favorite/AddOrRemoveFavorite.tsx
+++ client/src/features/car-details/car-data/add-or-remove-favorite/AddOrRemoveFavorite.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AddOrRemoveFavorite.module.css';
 import { usePost } from 'src/hooks/usePost';
 import { addOrRemoveFavorite } from 'src/api/services/favorites/FavoritesService';
@@ -9,6 +9,7 @@ interface Props {
 }
 
 const AddOrRemoveFavorite: React.FC<Props> = ({ carId, isFavorite }) => {
+  const [favorite, setFavorite] = useState(isFavorite);  
   const { mutate } = usePost<number>(
     'Favorite updated successfully', 
     true, 
@@ -17,17 +18,17 @@ const AddOrRemoveFavorite: React.FC<Props> = ({ carId, isFavorite }) => {
 
   const handleFavoritesClick = () => {
     mutate(carId);
-    window.location.reload();
+    setFavorite(!favorite);
   };
 
   return (
     <div 
       onClick={handleFavoritesClick} 
-      className={`${styles.favBtn} ${isFavorite ? styles.favorite : ''}`}
+      className={`${styles.favBtn} ${favorite ? styles.favorite : ''}`}
     >
       <i className="fa fa-heart"></i>
     </div>
   );
 };
 
-export default AddOrRemoveFavorite; 
+export default AddOrRemoveFavorite;

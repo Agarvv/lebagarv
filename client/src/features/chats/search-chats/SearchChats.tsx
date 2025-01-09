6ac0a49d4f4cc278f9ba50b397ec54ee9aef209e
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-09 20:21:44 +0000

    contacts filter

--- client/src/features/chats/search-chats/SearchChats.tsx
+++ client/src/features/chats/search-chats/SearchChats.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SearchChats.module.css' 
 import logo from 'src/logo.svg';
 import { useSelector } from 'react-redux';
@@ -6,22 +6,31 @@ import { RootState } from 'src/store';
 
 const SearchChats = () => {
     const contacts = useSelector((state: RootState) => state.chat.contacts);
+    const [filteredContacts, setFilteredContacts] = useState(contacts);
+
     const applyFilters = (title: string) => {
-        const newContacts = contacts.filter(c => c.car.title)
+        const newContacts = contacts.filter(c => c.car.title.toLowerCase().includes(title.toLowerCase()));
+        setFilteredContacts(newContacts);
+        console.log("filtered contacts", filteredContacts)
     }
-    return(
+
+    return (
         <>
         <div className={styles['user-img']}>
             <img src={logo} alt="Screenshot_20240928-011835.png"/>
         </div>
         <div className={styles['chats-search']}>
-            <input onChange={applyFilters(e.target.value)} type="text" placeholder="Search Contacts"/>
+            <input 
+                onChange={(e) => applyFilters(e.target.value)} 
+                type="text" 
+                placeholder="Search Contacts"
+            />
             <div className={styles.searchIcon}>
-              <i className="fa fa-search"></i>
+                <i className="fa fa-search"></i>
             </div>
         </div>
         </>
-    )
+    );
 }
 
-export default SearchChats; 
+export default SearchChats;

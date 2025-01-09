ecb5edc0bf3822f11d1e0bfa140218e94cd3dfd6
Agarvv <casluagarv@gmail.com>
2025-01-09 01:24:08 +0100

    issues

--- client/src/features/chatbox/Chatbox.tsx
+++ client/src/features/chatbox/Chatbox.tsx
@@ -9,13 +9,22 @@ import { getChatById } from 'src/api/services/chat/ChatService';
 import { SignalRContext } from "src/context/chat/SignalRContext";
 import { Message as MessageType } from 'src/types/chat/Message';
 import { Chat } from 'src/types/chat/Chat';
+import { RootState } from 'src/store/index';
+import { setChat } from 'src/store/chat/chatSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Chatbox = () => {
+    const dispatch = useDispatch(); 
     const { id } = useParams();
     const { connection } = useContext(SignalRContext);
+    
+    const setChatInRedux = (chat: Chat) {
+        dispatch(setChat(chat))
+    }
 
     const { data: chatData } = useGet<Chat>({
         serviceFunc: () => getChatById(Number(id)),
+        successFunc: (chat) => setChatInRedux(chat),
         withError: true
     });
 
@@ -23,7 +32,7 @@ const Chatbox = () => {
 
     useEffect(() => {
         if (chatData?.messages) {
-            setMessages(chatData?.messages); 
+            setMessages(chatData.messages); 
         }
     }, [chatData]);
 
@@ -36,7 +45,7 @@ const Chatbox = () => {
 
         return () => {
             connection.off("ReceiveMessage");
-        }; 
+        };
     }, [connection]);
 
     return (

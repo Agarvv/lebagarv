687ec5ebe64e666467755118a21b6a972d3556ca
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-09 20:19:25 +0000

    issues

--- client/src/features/chats/search-chats/SearchChats.tsx
+++ client/src/features/chats/search-chats/SearchChats.tsx
@@ -6,13 +6,16 @@ import { RootState } from 'src/store';
 
 const SearchChats = () => {
     const contacts = useSelector((state: RootState) => state.chat.contacts);
+    const applyFilters = (title: string) => {
+        const newContacts = contacts.filter(c => c.car.title)
+    }
     return(
         <>
         <div className={styles['user-img']}>
             <img src={logo} alt="Screenshot_20240928-011835.png"/>
         </div>
         <div className={styles['chats-search']}>
-            <input type="text" placeholder="Search Contacts"/>
+            <input onChange={applyFilters(e.target.value)} type="text" placeholder="Search Contacts"/>
             <div className={styles.searchIcon}>
               <i className="fa fa-search"></i>
             </div>
--- server/lebagarv/Presentation/Controllers/Auth/GoogleController.cs
+++ server/lebagarv/Presentation/Controllers/Auth/GoogleController.cs
@@ -2,31 +2,35 @@ namespace lebagarv.Presentation.Controllers.Auth
 {
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Authentication.Google;
-    using Microsoft.AspNetCore.Mvc; 
-
+    using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
 
     [ApiController]
     [Route("/api/lebagarv/auth/google")]
-    public class GoogleController : ControllerBase 
+    public class GoogleController : ControllerBase
     {
+
         [HttpGet]
         public IActionResult Login()
         {
-          return Challenge(new AuthenticationProperties 
-          { RedirectUri = "https://lebagarv.onrender.com/signin-google" }, GoogleDefaults.AuthenticationScheme);
+            var redirectUrl = Url.Action("Callback", "Google");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
         }
 
         [HttpGet("callback")]
         public IActionResult Callback()
-        {   
-           var result = HttpContext.AuthenticateAsync().Result;
-           if (result?.Principal != null)
-           {
-             var email = result.Principal.FindFirst("email")?.Value;
-             return Ok(new { message = "Authentication Success!", email });
-           }
+        {
+            
+            var result = HttpContext.AuthenticateAsync().Result;
 
-           return BadRequest("Error while authenticating");
+            if (result?.Principal != null)
+            {
+                var email = result.Principal.FindFirst(ClaimTypes.Email)?.Value;
+                return Ok(new { message = "Authentication Success!", email });
+            }
+
+            return BadRequest("Error while authenticating");
         }
     }
 }

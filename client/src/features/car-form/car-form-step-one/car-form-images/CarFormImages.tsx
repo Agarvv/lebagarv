6f7ac5940b4f44bad2c9f36c57ea4d5945fc17de
Agarvv <casluagarv@gmail.com>
2025-01-10 02:13:03 +0100

    create car page polish

--- client/src/features/car-form/car-form-step-one/car-form-images/CarFormImages.tsx
+++ client/src/features/car-form/car-form-step-one/car-form-images/CarFormImages.tsx
@@ -5,14 +5,14 @@ import styles from './CarFormImages.module.css';
 import useImageUpload from 'src/hooks/useImageUpload';
 
 const CarFormImages = () => {
-    const { setValue, watch } = useFormContext(); 
+    const { setValue, watch } = useFormContext();
     const { uploadImage } = useImageUpload();
-    const images = watch('images') || []; 
+    const images = watch('images') || [];
 
     const handleImageClick = () => {
         const fileInput = document.querySelector<HTMLInputElement>('#fileInput');
         if (images.length < 4 && fileInput) {
-            fileInput.click(); 
+            fileInput.click();
         }
     };
 
@@ -26,34 +26,44 @@ const CarFormImages = () => {
                     uploadedImages.push(imageUrl);
                 }
             }
-            setValue('images', [...images, ...uploadedImages]); 
+            setValue('images', [...images, ...uploadedImages]);
         }
     };
 
     const removeImage = (index: number) => {
-        const updatedImages = images.filter((_: any, i: any) => i !== index);
-        setValue('images', updatedImages); 
+        const updatedImages = images.filter((_, i) => i !== index);
+        setValue('images', updatedImages);
     };
 
     return (
         <div className={sharedCarFormStyles.field}>
             <label htmlFor="fileInput">Car Pictures</label>
-            <input 
-                type="file" 
-                multiple 
-                id="fileInput" 
-                onChange={handleFileChange} 
-                style={{ display: 'none' }} 
+            <input
+                type="file"
+                multiple
+                id="fileInput"
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
             />
             <div className={styles.carPictures}>
                 {[0, 1, 2, 3].map((index) => (
-                    <div key={index} className={styles.pictureSlot} onClick={handleImageClick}>
+                    <div
+                        key={index}
+                        className={styles.pictureSlot}
+                        onClick={handleImageClick}
+                    >
                         {images[index] ? (
                             <div className={styles.imageContainer}>
-                                <img src={images[index]} alt={`Car pic ${index + 1}`} />
-                                <button type="button" onClick={() => removeImage(index)} className={styles.removeBtn}>
-                                    R
-                                </button>
+                                <img
+                                    src={images[index]}
+                                    alt={`Car pic ${index + 1}`}
+                                />
+                                <div className={styles.removeBtn}>
+                                    <i
+                                        onClick={() => removeImage(index)}
+                                        className="fa fa-close"
+                                    ></i>
+                                </div>
                             </div>
                         ) : (
                             <span className={styles.placeholder}>+</span>

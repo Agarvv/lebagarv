f0baea8c334c440aadd0aa9c0b6d50a9c2bad343
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-11 16:17:14 +0000

    ui issues

--- server/lebagarv/obj/Debug/net8.0/lebagarv.AssemblyInfo.cs
+++ server/lebagarv/obj/Debug/net8.0/lebagarv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lebagarv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd548002fc6bc2d30773a1e5908c3407f6f6519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e489cb6e6eea0adc80ca0f7b9c2f2520069fe591")]
 [assembly: System.Reflection.AssemblyProductAttribute("lebagarv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lebagarv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4068b2b2d53689f4aac8cdba5460d5a04780f271
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-12 15:05:31 +0000

    Lebagar finished

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Header from 'src/layout/header/Header'
-import ContactsAside from 'src/layout/contacts-aside/ContactsAside'
 import CarLists from 'src/features/car-lists/CarLists'
 import styles from './HomePage.module.css'
 import AppLayout  from 'src/layout/AppLayout/AppLayout'

656f9a449ed10418ef1730f9f7a5b5f52ea731ea
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-12 15:06:45 +0000

    Lebagarv finished

--- client/src/pages/profile/ProfilePage.tsx
+++ client/src/pages/profile/ProfilePage.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import styles from './ProfilePage.module.css'
 import AppLayout from 'src/layout/AppLayout/AppLayout'
 import UserProfile from 'src/features/profile/UserProfile'
-import useProfile from 'src/hooks/useProfile'
 
 
 const ProfilePage = () => {
